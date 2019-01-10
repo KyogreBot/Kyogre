62fc1f2af29b77021d4d68947d9ce826a4554323
tehstone <stone.johnny@gmail.com>
2019-01-09 21:39:20 -0800

    needs more work

--- meowth/__main__.py
+++ meowth/__main__.py
@@ -1294,11 +1294,12 @@ async def on_raw_reaction_add(payload):
                 await _refresh_listing_channels_internal(guild, "research")
     raid_dict = guild_dict[guild.id].setdefault('raidchannel_dict', {})
     if channel.id in raid_dict:
-        raid_report = raid_dict[channel.id]
+        raid_report = channel.id
     else:
         raid_report = get_raid_report(guild, message.id)
     if raid_report is not None and user.id != Meowth.user.id:
-        if (raid_dict.get(raid_report, {}).get('reporter', 0) == payload.user_id or can_manage(user)):
+        reporter = raid_dict.get('reporter', 0)
+        if (raid_dict.get('reporter', 0) == payload.user_id or can_manage(user)):
             if str(payload.emoji) == '\u270f':
                 await modify_raid_report(payload, raid_report)
             elif str(payload.emoji) == '🚫':
